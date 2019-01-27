99f0000c312c44f8693554bbccf6274eca417329
Juan Alorro <jralorro93@gmail.com>
2019-01-27 20:20:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
-puts "Enter a number to check to see if that number is a prime"
-num = gets.chomp 
-
 def prime?(num)
+  puts "Enter a number to check to see if that number is a prime"
+  num = gets.chomp 
   if num % num == 1
     return true 
   else
