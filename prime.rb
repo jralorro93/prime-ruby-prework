e12360c30d8da3a75af087ed55a1ac2f5af9517a
Juan Alorro <jralorro93@gmail.com>
2019-01-27 20:22:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,3 @@
-def prime?(num)
-  puts "Enter a number to check to see if that number is a prime"
-  num = gets.chomp 
-  if num % num == 1
-    return true 
-  else
-    return false 
-  end 
-end 
+def prime?(n)
+  (2..Math.sqrt(n)).none? {|f| n % f == 0}
+end
